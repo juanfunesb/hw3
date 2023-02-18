6abd1d101e44dff77d43856c0adbbe3bf04a3512
juanfunesb <juan.funes@kellogg.northwestern.edu>
2023-02-18 22:52:15 +0000

    3rd adv

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,10 +4,19 @@ class PlacesController < ApplicationController
         @places=Place.all
     end
 
+    def show
+        @place = Place.find_by({"id" => params["id"]})
+    end
+
     def new
+      @place = Place.new  
     end
 
     def create
+        @place = Place.new  
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to"/places"
     end
 
 
